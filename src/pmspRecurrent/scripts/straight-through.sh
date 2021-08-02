15acfe1084bb840c552f3c115d97197a5ac917da
Ian Dennis Miller <ian@iandennismiller.com>
2021-08-02 18:00:34 +0000

    parallelize run

--- src/pmspRecurrent/scripts/straight-through.sh
+++ src/pmspRecurrent/scripts/straight-through.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=1 PMSP_PARTITION=0 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+function run_one_partition {
+    PARITION=$1
 
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=2 PMSP_PARTITION=0 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+    PMSP_RANDOM_SEED=1 PMSP_DILUTION=1 PMSP_PARTITION=$PARITION \
+        /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
 
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=3 PMSP_PARTITION=0 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+    PMSP_RANDOM_SEED=1 PMSP_DILUTION=2 PMSP_PARTITION=$PARITION \
+        /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
 
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=1 PMSP_PARTITION=1 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+    PMSP_RANDOM_SEED=1 PMSP_DILUTION=3 PMSP_PARTITION=$PARITION \
+        /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+}
 
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=2 PMSP_PARTITION=1 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
-
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=3 PMSP_PARTITION=1 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
-
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=1 PMSP_PARTITION=2 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
-
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=2 PMSP_PARTITION=2 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
-
-PMSP_RANDOM_SEED=1 PMSP_DILUTION=3 PMSP_PARTITION=2 \
-    /opt/Lens-linux/Bin/alens.sh scripts/straight-through.tcl
+run_one_partition 0 &
+run_one_partition 1 &
+run_one_partition 2 &
